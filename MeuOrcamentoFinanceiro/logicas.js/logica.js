3d74e0341dfaa1fbd5ba03ca4c8412a36188acce
DavyDev <silva.davybrenon@gmail.com>
2021-03-14 20:57:37 +0000

    Atualizado 14/03/2021

--- MeuOrcamentoFinanceiro/logicas.js/logica.js
+++ MeuOrcamentoFinanceiro/logicas.js/logica.js
@@ -442,18 +442,16 @@ function listaDeDados_2 (){
         switch (listaDeDados_2[i].categoria){
             case "1" : listaDeDados_2[i].categoria = "Salário"
                 break
-            
             case "2" : listaDeDados_2[i].categoria = "Alimentação"
                 break
-            case "3" : listaDeDados_2[i].categoria = "Educação"
+            case "3" : listaDeDados_2[i].categoria = "Edecação"
                 break
             case "4" : listaDeDados_2[i].categoria = "Lazer"
                 break
             case "5" : listaDeDados_2[i].categoria = "Saúde"
                 break
             case "6" : listaDeDados_2[i].categoria = "Transporte"
-                break
-            
+                break    
         }
         linha_2.insertCell(1).innerHTML = listaDeDados_2[i].categoria
         linha_2.insertCell(2).innerHTML = listaDeDados_2[i].descricao
